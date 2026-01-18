5d7d916e11d3ac93adc3bc6b137f0000ac189a56
Alexandre Parreira <ale.parreira@gmail.com>
2026-01-18 11:35:11 -0300

    Initial commit: Ralph TUI playground project

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Ralph TUI Playground
+// Um projeto simples para testar o workflow Ralph TUI + Beads + GitHub
+
+console.log("Hello from Ralph TUI Playground!");
+
+// TODO: Este projeto será expandido via Ralph TUI
